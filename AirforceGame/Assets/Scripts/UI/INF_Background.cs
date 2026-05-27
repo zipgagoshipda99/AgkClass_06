6a3238455f74f3754044379378d56f3546f83700
Grayson <broo28077@gmail.com>
2026-05-28 00:35:46 +0900

    오늘 한것 : 배경 무한 반복 구현. 무료 asset추가 (더 추가할려고 했으나 코드 이해를 우선순위로 해서 기능 하나만 추가)

---
+++ AirforceGame/Assets/Scripts/UI/INF_Background.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class INF_Background : MonoBehaviour
+{
+
+    [SerializeField] private float MoveSpeed = 2f;
+    private float backgroundImageHeight;
+
+    void Start()
+    {
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        backgroundImageHeight = sprite.texture.height / sprite.pixelsPerUnit;
+    }
+    void Update()
+    {
+        float moveY = Time.deltaTime * MoveSpeed;
+        //Time.deltatime == 이전 프레임부터 현재 프레임까지 걸린 시간(초)
+        //basically the time it takes for your computer to render a single frame.
+        
+        transform.position += new Vector3(0f, moveY, 0f);
+        if (Math.Abs(transform.position.y) - backgroundImageHeight >0f)
+        //절댓값 =  absolute value. 절댓값이 backgroundImage의 세의 길이보다 클때 리셋
+        {
+            transform.position = new Vector3(transform.position.x,0f,0f);
+        }
+    }
+}
